8efcc280b76daa00ab12b75f182bc23f4206495b
nerffan1 <ville2a@cmich.edu>
2023-08-15 20:14:15 -0400

    +++ Removed unused files +++

--- REQUEST.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-import requests
-
-resp = requests.get("https://www.nist.gov/sites/default/files/data.json")
-data = resp.json()
-i = 0
-print(data["@type"])
-print(data["describedBy"])
