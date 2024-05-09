9aaf371bfd70ddc951cb7556d5763861d43586be
nerffan1 <ville2a@cmich.edu>
2024-05-09 12:26:31 -0400

    0.3.2
    Added additional forms to read input

--- findfods.py
+++ findfods.py
@@ -12,27 +12,35 @@ dat = GlobalData()
 if len(sys.argv) == 1:
     print("No arguments were given, please provide XYZ file name")
     exit(1)
-
-# Read a file and get parameters for multiple molecules/systems
-elif sys.argv[1] == "list":
-    assert len(sys.argv) == 3, "You did not provide a list of files to analyze."
-    print("You provided the 'list' flag. The following file is expected to have several filenames")
-    mols = CreateMolecules(sys.argv[2])
-    #GridPropRatios(mols)
-    Histogram_Radii(mols)
-    Histogram_Deviation(mols)
-    #Angles_Hist(mols)
-
-# Determine parameters just for one molecule
 elif len(sys.argv) == 2:
     print("One argument passed. Creating FOD Prediction.")
     mol = Molecule(sys.argv[1])
     mol.CreateCLUSTER()
     mol.CreateFRMORB()
-    mol._debug_printBFODsXYZ()
-
-# Determine parameters and compare with a target file of FRMORBs
+    mol.CreateXYZ()
 elif len(sys.argv) == 3:
-    print("Two arguments passed. Reverse Determination of Relaxed FODs.")
-    mol = Molecule(sys.argv[1], sys.argv[2])
-    mol._debug_printBFODsXYZ()
+    if sys.argv[1] == "list":
+        assert len(sys.argv) == 3, "You did not provide a list of files to analyze."
+        print("You provided the 'list' flag. Your input file is expected to have several filenames for comparison.")
+        mols = CreateMolecules(sys.argv[2])
+        for m in mols:
+            m.GeFBEdges()
+        EdgeDist_FFODs(mols)
+        Angles_Hist(mols)
+
+    elif sys.argv[1] == "check":
+        mol = Molecule(sys.argv[2])
+        mol._CheckChemValency()
+
+    elif sys.argv[1] == "createsdf":
+        mol = Molecule(sys.argv[2])
+        mol.CreateSDF()
+
+    else:
+        print("Two arguments passed. Reverse Determination of Relaxed FODs.")
+        mol = Molecule(sys.argv[1], sys.argv[2])
+        mol.CreateCompXYZ()
+        mol.GeFBEdges()
+        EdgeDist_FFODs([mol])
+        #mol._debug_printBFODsXYZ()
+        #mol._debug_printAtoms()
