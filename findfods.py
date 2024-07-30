9fc1213d9b966671431f8d3ba0e6aa47cf69684c
nerffan1 <ville2a@cmich.edu>
2024-07-30 19:01:26 -0400

    Analysis Tools
    MolecularSet.py:
    - This class holds an array of molecules, or structures, to be analyzed.
    - It is meant to automate the information obtained from the <molecule>
    objects.
    - The development of this class requires to take into account the
    <Molecule> class, and vice versa, so that this class is efficient in
    function calls.
    - A lot of the tooling was developed for Angel Villegas MS Thesis.

--- findfods.py
+++ findfods.py
@@ -12,21 +12,27 @@ dat = GlobalData()
 if len(sys.argv) == 1:
     print("No arguments were given, please provide XYZ file name")
     exit(1)
+
 elif len(sys.argv) == 2:
     print("One argument passed. Creating FOD Prediction.")
     mol = Molecule(sys.argv[1])
     mol.CreateCLUSTER()
     mol.CreateFRMORB()
     mol.CreateXYZ()
+    #mol._debug_printAtoms()
+
 elif len(sys.argv) == 3:
     if sys.argv[1] == "list":
         assert len(sys.argv) == 3, "You did not provide a list of files to analyze."
         print("You provided the 'list' flag. Your input file is expected to have several filenames for comparison.")
         mols = CreateMolecules(sys.argv[2])
-        for m in mols:
-            m.GeFBEdges()
-        EdgeDist_FFODs(mols)
-        Angles_Hist(mols)
+        MolecularSet(mols).saveset('duyen.pickle')
+        #EdgeDist_FFODs(mols)
+        #Angles_Hist(mols)
+        #tables(mols)
+        # Histogram_Radii(mols)
+        #Histogram_Deviation(mols)
+        #ffod_radii()
 
     elif sys.argv[1] == "check":
         mol = Molecule(sys.argv[2])
@@ -40,7 +46,6 @@ elif len(sys.argv) == 3:
         print("Two arguments passed. Reverse Determination of Relaxed FODs.")
         mol = Molecule(sys.argv[1], sys.argv[2])
         mol.CreateCompXYZ()
-        mol.GeFBEdges()
-        EdgeDist_FFODs([mol])
+        #mol.GeFBEdges()
         #mol._debug_printBFODsXYZ()
         #mol._debug_printAtoms()
